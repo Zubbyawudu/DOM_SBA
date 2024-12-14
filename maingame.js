0c2b3c69e1e699be90fe71275402e46c2af22ef9
Zubairu Awudu <zubairuawudu123@gmail.com>
2024-12-14 02:10:19 -0500

    DOM SBA

--- maingame.js
+++ maingame.js
@@ -1,35 +1,29 @@
 const logoContainer = document.querySelector(".logocontainer");
+const options = document.querySelectorAll(".options");
+const answerContainer = document.querySelector(".answercontainer");
+const previousbtn = document.querySelector(".previous");
 const nextbtn = document.querySelector(".next");
+const score = document.querySelector(".score");
+const scoreContainer = document.querySelector(".scorecontainer");
+
+let scoreCount = 0;
 
 
-// Alert SECTION
+
+// Alert Section
 const welcomeMessage = () => {
   alert('Welcome to the BRAND QUIZ GAME');
 
 }
 welcomeMessage();
 
-//image section
-const image = document.createElement("img");
-image.src = "https://images.unsplash.com/photo-1619551734325-81aaf323686c?q=80&w=1049&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-image.alt = "image";
-image.classList
-logoContainer.appendChild(image);
-image.style.width = "100%";
-image.style.height = "100%";
+//Logos Array section 
+const logosArray = [{
+  image: "https://i.pinimg.com/736x/9d/d7/5f/9dd75f3cb13b4680260c8a67857ae34c.jpg",
+  options: ["Infinity", "Lamborghini", "Ferrari", "Tesla"],
+  correct: "Infinity"
+}
+  
+]
 
-//options section
-const options = document.querySelectorAll(".options");
-const answerContainer = document.querySelector(".answercontainer");
-const previousbtn = document.querySelector(".previous");
-const score = document.querySelector(".score");
-const scoreContainer = document.querySelector(".scorecontainer");
-let scoreCount = 0;
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    
-    
-    score.textContent = `Score: ${scoreCount}`;
-  });
-});
+//options Section
